09823c2fe56313a5964111f8008efa08c4302845
AJPreto <martinsgomes.jose@gmail.com>
2024-12-15 02:35:00 +0000

    Forms working

--- lauetoze/app.py
+++ lauetoze/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")  # Render the index.html template
 
+@app.route("/rsvp", endpoint='rsvp')
+def rsvp():
+    return render_template("rsvp.html")  # Render the index.html template
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000,debug=True)
