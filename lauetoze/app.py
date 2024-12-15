d9f2046c97b2c091a88f2e771dde87bd318f9bb3
AJPreto <martinsgomes.jose@gmail.com>
2024-12-15 01:16:50 +0000

    Adding working example

--- lauetoze/app.py
+++ lauetoze/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is a Flask app running on EC2."
+    return render_template("index.html")  # Render the index.html template
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000,debug=True)
